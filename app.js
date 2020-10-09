94dadb17879f63fd2c927edebcd77ee97b7ccbb9
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-10-09 18:41:44 +0000

    Created the function to make both boards, user and computer

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 document.addEventListener ('DOMContentLoaded', () => {
 
     const userGrid = document.querySelector('.grid-user');
-    const computerGrid = document.querySelector('grid-computer');
-    const displayGrid = document.querySelector('grid-display');
+    const computerGrid = document.querySelector('.grid-computer');
+    const displayGrid = document.querySelector('.grid-display');
     const ships = document.querySelector('.ship');
     const destroyer = document.querySelector('.destroyer-container');
     const submarine = document.querySelector('.submarine-container');
@@ -15,5 +15,21 @@ document.addEventListener ('DOMContentLoaded', () => {
     const rotateButton = document.querySelector('#rotate');
     const turnDisplay = document.querySelector('#whose-go');
     const infoDisplay = document.querySelector('#info');
-    
+    const userSquares = [];
+    const computerSquares = [];
+
+    const width = 10; 
+
+    //create board
+    function createBoard (grid, squares, width) {    // we will pass 3 parameters , the grid, squares and the width
+        for ( let i = 0; i < width*width; i++){
+            const square = document.createElement('div');  // we store in square each div element created 
+            square.dataset.id = i;  // each time it loops we will set one unique id ( we gonna get 99 divs) 
+            grid.appendChild(square); //we append div to our userGrid
+            squares.push(square);     // each time we crate one div, we add it to this array to keep tracking
+        }
+    }
+
+    createBoard(userGrid, userSquares, width);
+    createBoard(computerGrid, computerSquares, width);
 })
