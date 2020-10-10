55e26e585649ddbda16cdf2a6f1e3461be8ce90d
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-10-10 00:59:37 +0000

    created boards, ship array and draw the ships in random locations for the computer

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener ('DOMContentLoaded', () => {
     const userGrid = document.querySelector('.grid-user');
     const computerGrid = document.querySelector('.grid-computer');
     const displayGrid = document.querySelector('.grid-display');
-    const ships = document.querySelector('.ship');
+    const ship = document.querySelector('.ship');
     const destroyer = document.querySelector('.destroyer-container');
     const submarine = document.querySelector('.submarine-container');
     const cruiser = document.querySelector('.cruiser-container');
@@ -21,7 +21,7 @@ document.addEventListener ('DOMContentLoaded', () => {
     const width = 10; 
 
     //create board
-    function createBoard (grid, squares, width) {    // we will pass 3 parameters , the grid, squares and the width
+    function createBoard (grid, squares) {    // we will pass 3 parameters , the grid, squares and the width
         for ( let i = 0; i < width*width; i++){
             const square = document.createElement('div');  // we store in square each div element created 
             square.dataset.id = i;  // each time it loops we will set one unique id ( we gonna get 99 divs) 
@@ -30,6 +30,70 @@ document.addEventListener ('DOMContentLoaded', () => {
         }
     }
 
-    createBoard(userGrid, userSquares, width);
-    createBoard(computerGrid, computerSquares, width);
+    createBoard(userGrid, userSquares);
+    createBoard(computerGrid, computerSquares);
+
+    //Ships
+    //We go create one array of objects, in this case, ships
+
+    const shipsArray = [
+        {
+            name: 'destroyer',
+            directions: [
+                [0, 1],     //horizontally
+                [0, width]  //vertically 
+            ]
+        },
+        {
+            name: 'submarine',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'cruiser',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'battleship',
+            directions: [
+                [0, 1, 2, 3],
+                [0, width, width*2, width *3]
+            ]
+        },
+        {
+            name: 'carrier',
+            directions: [
+                [0, 1, 2, 3, 4],
+                [0, width, width*2, width *3, width*4]
+            ]
+        },
+    ]
+
+    //draw the ships in random locations for the computer
+
+    function generate(ship){
+
+        let randomDirection = Math.floor(Math.random() * ship.directions.length)  //we generate a number between each ship length
+        let current = ship.directions[randomDirection]     // we grab the ship direction , vertical or horizontal 
+        
+        if (randomDirection === 0){
+            directions = 1;  // we will render our ship horizontally
+        }
+
+        if (randomDirection === 1){
+            directions = 10;   // we will render our ship downwards 
+        }
+
+        let randomStart = Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length) * directions);  // we want a point to start, we know that length is 100 and we need to sub the length of 1 grid ( if we get 100 the ship will start offscreen)
+    }
+
+    //make sure that we dont get a taken square
+    const isTaken = current.some(index => computerSquares [randomStart + index].classList.contains('taken'));
+    const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1);
+
 })
